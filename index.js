3d3a7f0913aa660b2124c0fa263e9d4c980c1220
Luke Ramljak <lukeramljak@proton.me>
2023-08-20 21:36:20 +0800

    Add player/computer scoring

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+
   if (playerSelection == computerSelection) {
     return "It's a tie!";
   } else if (playerSelection == 'rock' && computerSelection == 'paper'
     || playerSelection == 'paper' && computerSelection == 'scissors'
     || playerSelection == 'scissors' && computerSelection == 'rock'
   ) {
+    computerScore += 1;
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   } else {
+    playerScore += 1;
     return `You win! ${playerSelection} beats ${computerSelection}`;
   }
 }
+
+let playerScore;
+let computerScore;
 
 function game() {
   let i = 5;
@@ -24,6 +30,13 @@ function game() {
     const computerSelection = getComputerChoice().toLowerCase();
     console.log(playRound(playerSelection, computerSelection));
     i--;
+  }
+  if (playerScore == computerScore) {
+    return "It's a tie!";
+  } else if (playerScore < computerScore) {
+    return "You lose!";
+  } else {
+    return "You win!";
   }
 }
 
