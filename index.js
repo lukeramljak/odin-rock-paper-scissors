4f326d6d2dd4419cfc183c8d34645085544c3748
Luke Ramljak <lukeramljak@proton.me>
2023-08-20 21:17:13 +0800

    Add playRound function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function getComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissors'];
   const index = Math.floor(Math.random() * choices.length);
-  return choices[index]
+  return choices[index];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "It's a tie!";
+  } else if (playerSelection == 'rock' && computerSelection == 'paper'
+    || playerSelection == 'paper' && computerSelection == 'scissors'
+    || playerSelection == 'scissors' && computerSelection == 'rock'
+  ) {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  }
 }
