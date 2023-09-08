2cc765956b946dd33fcf5364c2042a77f4bb1632
Luke Ramljak <lukeramljak@proton.me>
2023-09-08 23:56:15 +0800

    refactor: toggle tailwind class

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-const restart = document.getElementById('restart');
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const restart = document.getElementById("restart");
 
-const playerScoreElement = document.getElementById('player-score');
-const computerScoreElement = document.getElementById('computer-score');
-const resultElement = document.getElementById('result');
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
+const resultElement = document.getElementById("result");
 
 let playerScore = 0;
 let computerScore = 0;
 
-rock.addEventListener('click', () => playRound('Rock', getComputerChoice()));
-paper.addEventListener('click', () => playRound('Paper', getComputerChoice()));
-scissors.addEventListener('click', () => playRound('Scissors', getComputerChoice()));
-restart.addEventListener('click', () => restartGame());
+rock.addEventListener("click", () => playRound("Rock", getComputerChoice()));
+paper.addEventListener("click", () => playRound("Paper", getComputerChoice()));
+scissors.addEventListener("click", () =>
+  playRound("Scissors", getComputerChoice()),
+);
+restart.addEventListener("click", () => restartGame());
 
 function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors'];
+  const choices = ["Rock", "Paper", "Scissors"];
   const index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
@@ -27,9 +29,11 @@ function playRound(playerChoice, computerChoice) {
     return;
   }
 
-  if (playerChoice == 'Paper' && computerChoice == 'Rock'
-    || playerChoice == 'Scissors' && computerChoice == 'Paper'
-    || playerChoice == 'Rock' && computerChoice == 'Scissors') {
+  if (
+    (playerChoice == "Paper" && computerChoice == "Rock") ||
+    (playerChoice == "Scissors" && computerChoice == "Paper") ||
+    (playerChoice == "Rock" && computerChoice == "Scissors")
+  ) {
     playerScore += 1;
     resultElement.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
   } else {
@@ -43,16 +47,16 @@ function playRound(playerChoice, computerChoice) {
 
 function checkIfGameOver() {
   if (playerScore == 5 || computerScore == 5) {
-    resultElement.textContent = 'Game over. Play again?';
-    setButtonStates('none', 'block');
+    resultElement.textContent = "Game over. Play again?";
+    toggleButtonStates("hidden");
   }
 }
 
-function setButtonStates(state1, state2) {
-  rock.style.display = state1;
-  paper.style.display = state1;
-  scissors.style.display = state1;
-  restart.style.display = state2;
+function toggleButtonStates(state) {
+  rock.classList.toggle(state);
+  paper.classList.toggle(state);
+  scissors.classList.toggle(state);
+  restart.classList.toggle(state);
 }
 
 function setScoreTextContent() {
@@ -63,7 +67,7 @@ function setScoreTextContent() {
 function restartGame() {
   playerScore = 0;
   computerScore = 0;
-  resultElement.textContent = 'Ready!';
+  resultElement.textContent = "Ready!";
   setScoreTextContent();
-  setButtonStates('block', 'none');
+  toggleButtonStates("hidden");
 }
